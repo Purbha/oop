508312a49fc88413379418087887e56070931981
Irsyad <purbha_6string@yahoo.co.id>
2021-05-11 10:43:15 +0700

    UPDATE 20210511 10:43

--- src/oop/lat_game/Test1.java
+++ src/oop/lat_game/Test1.java
@@ -1,24 +1,57 @@
 package oop.lat_game;
+
+import java.util.Scanner;
+
 /* @author Irsyad */
 public class Test1 {
     
+    void Garis(int jml) {
+        for(int i=0;i<jml;i++) {
+            System.out.print("=");        
+        }
+        System.out.print("\n");
+    }
+    
+    void Tampil(int Pilih) {
+        switch (Pilih) {
+            case 1:
+                Zombie zombie = new Zombie("Joong-ki",120,35);
+                zombie.attack();
+                zombie.Jalan();
+                zombie.Menggigit();
+                zombie.garis();                
+                break;
+            case 2:
+                Monster monster = new Monster("Jisoo", 95, 65);
+                monster.attack();
+                monster.Lompat();
+                monster.Memukul();
+                monster.garis();
+                break;
+            default:
+                Burung burung = new Burung("Jennie", 80, 75);
+                burung.attack();
+                burung.Lompat();
+                burung.Terbang();
+                burung.Jalan();
+                burung.garis();
+                break;
+        }        
+    }
+    
     public static void main(String[] args) {
-        Zombie zombie = new Zombie("Joong-ki",120,35);
-        zombie.attack();
-        zombie.Jalan();
-        zombie.Menggigit();
-        zombie.garis();
-        Monster monster = new Monster("Jisoo", 95, 65);
-        monster.attack();
-        monster.Lompat();
-        monster.Memukul();
-        monster.garis();
-        Burung burung = new Burung("Jennie", 80, 75);
-        burung.attack();
-        burung.Lompat();
-        burung.Terbang();
-        burung.Jalan();
-        burung.garis();
+        Test1 test1 = new Test1();
+        System.out.println("Program OOP");
+        test1.Garis(30);
+        System.out.println("Pilihan Musuh");
+        System.out.println("1. Zombie Joong-ki");
+        System.out.println("2. Monster Jisoo");
+        System.out.println("3. Burung Jennie");
+        test1.Garis(30);
+        System.out.print("Pilih Musuh [1/2/3]: ");
+        Scanner scanner = new Scanner(System.in);
+        int pilih = scanner.nextInt();
+        test1.Tampil(pilih);
     }
     
 }
