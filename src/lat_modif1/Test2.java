aeb42b801a116de9bcf3e8d85d16129910765e0b
Irsyad Purbha <irsyadpurbha@gmail.com>
2022-04-06 12:42:01 +0700

    UPDATE 20220406 12:41

--- src/lat_modif1/Test2.java
+++ src/lat_modif1/Test2.java
@@ -7,7 +7,6 @@ public class Test2 {
     public static void main(String[] args) {
         System.out.println("Akses Di Package Yang Sama");
         Lat2 obj = new Lat2();
-        
     }
     
 }
--- src/lat_modif3/Test1.java
+++ src/lat_modif3/Test1.java
@@ -1,33 +1,27 @@
 package lat_modif3;
 /* @author Irsyad */
-/*
-    Meng-extends class Lat1 akan menyebabkan error karena class Lat1
-    bertipe final.
-*/
+// Meng-extends class Lat1 akan menyebabkan error karena class Lat1 bertipe final.
 //public class Test1 extends Lat1{
-public class Test1{
-    
+public class Test1{   
     //Hanya bisa bisa diakses dalam class yang sama
-    private String namaModel;
-    
+    private String namaModel;  
     public static void main(String[] args) {
         Test1 mobil1 = new Test1();
         mobil1.namaModel = "MOBILIO";
-        /*
-            Method Klakson tidak ada di class Test1, adanya di
-            class Lat1.
-        */
-        //mobil1.Klakson();
-        /*
-            Variable merk tidak ada di class Test1, adanya di
-            class Lat1.
-        */
-        //System.out.println(mobil1.merk + " " + mobil1.namaModel);
+/*
+Method Klakson tidak ada di class Test1, adanya di class Lat1. "mobil1.Klakson()"
+hanya bisa dijalankan klo kelasnya bisa di inheritance. Walaupun method
+"Klakson" tipenya world tapi classnya tidak bisa dibuat object.
+*/
+//mobil1.Klakson();  
+/*
+Variable merk tidak ada di class Test1, adanya di class Lat1. Walaupun attributnya
+protected yang artinya bisa diakses dalam package yang sama tapi classnya tidak bisa
+dibuat object.
+*/
+//System.out.println(mobil1.merk + " " + mobil1.namaModel);
         System.out.println(mobil1.namaModel);
-        
-        //==========================================//
-        Lat1 obj1 = new Lat1();
-        
+        Lat1 obj1 = new Lat1();   
     }
     
 }
